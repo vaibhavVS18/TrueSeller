a2257600b55230ea865d376b20df59a89726d69c
vaibhav <vaibhav.iiituna1111@gmail.com>
2025-11-04 23:41:59 +0530

    loader for images

--- frontend/src/components/home/HeroSection1.jsx
+++ frontend/src/components/home/HeroSection1.jsx
@@ -1,33 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import {FaArrowRight, FaArrowDown} from "react-icons/fa";
 import {motion} from "framer-motion";
 
 const HeroSection1 = ({isLeftHovered, setIsLeftHovered}) => {
 
-  const [bgLoaded, setBgLoaded] = useState(false);
-
-  useEffect(() => {
-    if (isLeftHovered) {
-      const img = new Image();
-      img.src = "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275346/bs5_n7u95t.png";
-      img.onload = () => setBgLoaded(true);
-    } else {
-      setBgLoaded(false);
-    }
-  }, [isLeftHovered]);
-
   return (
     // <section className="flex flex-col items-center justify-center lg:items-start w-full text-center lg:text-left px-4 sm:px-6 lg:py-0">
-    <section
-      className={`relative flex flex-col flex-1 h-full items-center justify-between text-center lg:text-left sm:px-2
-        transition duration-500 bg-cover bg-center`}
-      style={{
-        backgroundImage: bgLoaded
-          ? "url('https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275346/bs5_n7u95t.png')"
-          : "", // placeholder
-      }}
-    >
+      <section
+        className={`relative flex flex-col flex-1 h-full items-center justify-between text-center lg:text-left sm:px-2 
+        ${isLeftHovered ? "bg-[url('https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275346/bs5_n7u95t.png')]" : "" } 
+        bg-cover bg-center transition duration-200`}
+      >
       
       {/* Brand Name */}
       <h2 className="md:absolute top-16 text-4xl md:text-3xl font-extrabold mb-2 mt-5 md:mt-0 tracking-tight"
@@ -83,26 +67,6 @@ const HeroSection1 = ({isLeftHovered, setIsLeftHovered}) => {
         sale. 🚀
       </p>
 
-      {/* Buttons */}
-      {/* <div className="flex gap-3 justify-center lg:justify-start mt-1 sm:mt-2"> */}
-        {/* <Link 
-          className="px-4 py-3 sm:px-5 sm:py-2.5 
-                     bg-gradient-to-r from-emerald-500 to-cyan-500 
-                     hover:from-emerald-400 hover:to-cyan-400 
-                     text-white font-semibold rounded-xl shadow-md 
-                     active:scale-95 transition-all text-sm sm:text-base"
-          to="/start-shop"
-        >
-          ⚡ Start your Shop
-        </Link>
-        <Link 
-          className="px-4 py-3 sm:px-5 sm:py-2.5 bg-white border border-gray-300 text-gray-700 hover:bg-gray-50 font-semibold rounded-xl shadow-md active:scale-95 transition-all text-sm sm:text-base"
-          to="/shopsPage"
-        >
-          📈 Explore Shops
-        </Link> */}
-      {/* </div> */}
-
       {
       <div className={`${isLeftHovered ? "invisible" : ""} flex flex-1 items-end h-full transition duration-100`}>
         <img src="https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275346/bs4_xrstpy.png" className="max-h-[82vh] md:max-w-[87vh] object-contain"
@@ -110,23 +74,6 @@ const HeroSection1 = ({isLeftHovered, setIsLeftHovered}) => {
         ></img>
       </div>
       }
-
-{/* 
-        <div className="mt-6 flex justify-between gap-4 max-w-xl h-[28vh] mx-auto">
-        {[
-          {img:"/mid.png", text:"Become a Seller", link:"/start-shop"},
-          {img:"/mid.png", text:"Explore Shops", link:"/shopsPage"},
-          {img:"/mid.png", text:"Explore Products", link:"/productsPage"},
-        ].map((item,i)=>(
-          <Link href={item.link} key={i}
-            className="flex flex-col w-1/3 group bg-white shadow-md rounded-xl p-4 flex flex-col items-center 
-            transition hover:shadow-xl hover:-translate-y-1 border border-gray-200"
-          >
-            <img src={item.img} className="mb-1 group-hover:scale-110 transition object-contain" />
-            <span className="font-semibold text-gray-700">{item.text}</span>
-          </Link>
-        ))}
-      </div> */}
 
     </section>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import HeroSection1 from "../components/home/HeroSection1";
 import { UserContext } from "../context/user.context";
@@ -6,81 +6,129 @@ import SecondSection from "../components/SecondSection";
 import ExploreCategories from "../components/ExploreCategories";
 
 const Home = () => {
-    const navigate = useNavigate();
-    const { user , setUser} = useContext(UserContext);
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
 
-    const [isLeftHovered, setIsLeftHovered] = useState(false);
+  const [isLeftHovered, setIsLeftHovered] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-      fetch("https://trueseller-q39b.onrender.com");
-      fetch("https://codchat-jvsc.onrender.com");
-    }, []);
+  const importantImages = [
+    "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275346/bs4_xrstpy.png",
+    "https://res.cloudinary.com/dmfdw5lzn/image/upload/f_auto,q_auto,w_1200/v1762275346/bs5_n7u95t.png",
+    "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275348/left_cpy0vq.png",
+    "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275348/left1_pugqlr.png",
+    "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275349/right_xpdyik.png",
+    "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275349/right1_mktnnw.png",
+  ];
 
-    useEffect(()=>{
-      const params = new URLSearchParams(window.location.search);   // it gives query string part of the url
-      const token = params.get("token");
-      const redirectPage= params.get("redirectPage");
-      console.log(redirectPage);
+  useEffect(() => {
+    let loadedCount = 0;
 
-      if (token) {
-        // Save token in localStorage
-        localStorage.setItem("token", token);
+    importantImages.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => {
+        loadedCount += 1;
+        if (loadedCount === importantImages.length) {
+          setLoading(false); // all done!
+        }
+      };
+      img.onerror = () => {
+        loadedCount += 1;
+        if (loadedCount === importantImages.length) {
+          setLoading(false); // continue even if one fails
+        }
+      };
+    });
+  }, []);
 
-        // Navigate to original page
-        navigate(redirectPage, { replace: true });
-      }
-    }, []);
+  useEffect(() => {
+    fetch("https://trueseller-q39b.onrender.com");
+    fetch("https://codchat-jvsc.onrender.com");
+  }, []);
 
-return (
-  
-  <>
-       <div className="flex flex-col items-between w-full h-[86vh] sm:h-[91vh] border-b">
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
+    const redirectPage = params.get("redirectPage");
+    console.log(redirectPage);
+
+    if (token) {
+      localStorage.setItem("token", token);
+      navigate(redirectPage, { replace: true });
+    }
+  }, [navigate]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-black text-white">
+        <div className="flex flex-col items-center gap-4">
+          <div className="w-12 h-12 border-4 border-cyan-400 border-t-transparent rounded-full animate-spin"></div>
+          <p className="text-lg font-medium">Loading Awesome Experience...</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex flex-col items-between w-full h-[86vh] sm:h-[91vh] border-b">
         <div className="flex justify-between h-full">
-
-          <div className="hidden h-[90.9vh] sm:flex items-center justify-center"
+          {/* Left Hero */}
+          <div
+            className="hidden h-[90.9vh] sm:flex items-center justify-center"
             onMouseEnter={() => setIsLeftHovered(true)}
             onMouseLeave={() => setIsLeftHovered(false)}
           >
-            <img 
-              src={isLeftHovered ? "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275348/left_cpy0vq.png" : "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275348/left1_pugqlr.png"}
+            <img
+              src={
+                isLeftHovered
+                  ? "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275348/left_cpy0vq.png"
+                  : "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275348/left1_pugqlr.png"
+              }
               alt="left hero"
               className="h-full object-contain"
               loading="lazy"
             />
           </div>
+
+          {/* Center Hero Section */}
           <div className="flex flex-1 justify-center">
-            <HeroSection1 isLeftHovered={isLeftHovered} setIsLeftHovered={setIsLeftHovered} />
+            <HeroSection1
+              isLeftHovered={isLeftHovered}
+              setIsLeftHovered={setIsLeftHovered}
+            />
           </div>
 
-          <div className="hidden h-[90.9vh] sm:flex items-center"
+          {/* Right Hero */}
+          <div
+            className="hidden h-[90.9vh] sm:flex items-center"
             onMouseEnter={() => setIsLeftHovered(true)}
             onMouseLeave={() => setIsLeftHovered(false)}
           >
-            <img 
-              src={isLeftHovered ? "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275349/right_xpdyik.png" : "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275349/right1_mktnnw.png"}
+            <img
+              src={
+                isLeftHovered
+                  ? "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275349/right_xpdyik.png"
+                  : "https://res.cloudinary.com/dmfdw5lzn/image/upload/v1762275349/right1_mktnnw.png"
+              }
               alt="right hero"
               className="h-full object-contain"
               loading="lazy"
             />
           </div>
-
         </div>
       </div>
 
+      {/* Second Section */}
+      <SecondSection />
 
-      {/* second section of home page */}
-        <SecondSection/> 
-
-
-        {/* Third section */}
+      {/* Third Section */}
       <div className="flex w-full md:h-[90vh] border-t">
         <ExploreCategories />
       </div>
-  </>
-
-);
-
-}
-
+    </>
+  );
+};
 
 export default Home;
